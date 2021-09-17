75316e3938075fdfd4bf74b55c4c0896cf0d3fa7
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-09-18 01:05:06 +0530

    initial commit

--- src/arrays/AlterNatePositiveNegative.java
+++ src/arrays/AlterNatePositiveNegative.java
@@ -2,33 +2,39 @@ package arrays;
 import java.util.Arrays;
 public class AlterNatePositiveNegative {
     private  static  int[] getAlternateArray(int[] arr) {
-        int[] array1 = new int[arr.length];
-        int[] array2=new int[arr.length];
+        int[] possitive = new int[arr.length];
+        int[] negative=new int[arr.length];
         int k = 0;
+        int r=0;
         for (int i=0;i<arr.length;i++)
         {
-            if(arr[i]>0)
+            if(k%2==0)
             {
-                array1[k]=arr[i];
+                possitive[k++]=arr[i];
+            }
+            else {
+                negative[k++]=arr[i];
             }
         }
         int m = 0;
-        for (int i=0;i<arr.length;i++)
-        {
-            if(arr[i]<0)
-            {
-                array2[m]=arr[i];
-            }
-        }
-        int l=0,r=0;
+        int l=0;
         int n=0;
         while (l<k&&m<r){
-            arr[n++]=array1[l++];
-            arr[n++]=array2[r++];
+            if (n%2==0)
+            {
+                arr[n++]=possitive[l++];
+            }
+            else {
+                arr[n++] =negative[m++];
+            }
         }
         while (l<k)
         {
-            arr[n++]=array1[l++];
+            arr[n++]=possitive[l++];
+        }
+        while (m<r)
+        {
+            arr[n++]=possitive[m++];
         }
         return arr;
     }
