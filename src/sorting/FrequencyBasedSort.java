a153b4c09283098e870771b050be67abeb2905d8
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-09-19 12:25:32 +0530

    update

--- src/sorting/FrequencyBasedSort.java
+++ src/sorting/FrequencyBasedSort.java
@@ -4,24 +4,59 @@
  *
  * Input: arr[] = {2, 5, 2, 6, -1, 9999999, 5, 8, 8, 8}
  * Output: arr[] = {8, 8, 8, 2, 2, 5, 5, 6, -1, 9999999}
-
  */
-
 package sorting;
-
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class FrequencyBasedSort {
-    /*private static List<Integer> frequencyBasedSort(List<Integer> list)
+    private static List<Integer> frequencyBasedSort(List<Integer> list,Integer[] arr)
     {
+        Map<Integer,Integer> mapCount=new HashMap<>();
+        Map<Integer,Integer> mapIndex=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+          if(mapCount.containsKey(arr[i]))
+          {
+              mapCount.put(arr[i], mapCount.get(arr[i])+1);
+          }
+          else {
+              mapCount.put(arr[i],1);
+              mapIndex.put(arr[i],i);
+          }
+        }
 
-    }*/
+
+        // System.out.println(mapCount);
+        //System.out.println(mapIndex);
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2)
+            {
+                /*{2=2, 5=2, 6=1, 8=3}
+                {2=0, 5=1, 6=5, 8=3}*/
+               // Input:  arr[] = {2, 5, 2, 8, 5, 6, 8, 8}
+                System.out.println(list);
+                int frq1=mapCount.get(o1);
+                int frq2=mapCount.get(o2);
+                if(frq1!=frq2)
+                {
+                    return frq2-frq1;
+                }
+                else {
+                    return mapIndex.get(o1)-mapIndex.get(o2);
+
+                }
+
+
+            }
+        });
+        return list;
+
+    }
     public static void main(String[] args) {
-        int[] arr={2, 5, 2, 8, 5, 6, 8, 8};
-        Arrays.asList(arr);
-        //frequencyBasedSort(list);
-
-
+        Integer[] arr={2, 5, 2, 8, 5, 6, 8, 8};
+        List<Integer> list=Arrays.asList(arr);
+        List<Integer> result =frequencyBasedSort(list,arr);
+        System.out.println(result);
     }
 }
